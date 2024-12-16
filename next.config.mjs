024bae42d97c7445d7c37d204bbf1076b03e34e8
andres <andresnowak52@gmail.com>
2024-12-16 19:09:20 +0100

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
+const repoName = "/robodatapioneers2024_datastory";
 
 const nextConfig = {
-	output: "export", // Enables static HTML export
+	output: "export",
 	images: {
-		unoptimized: true, // Ensures images work without external optimization servers
+		unoptimized: true,
 	},
-	assetPrefix: "./", // Ensures assets have the correct relative paths for GitHub Pages
-	trailingSlash: true, // Adds trailing slashes to URLs for GitHub Pages compatibility
-	basePath: isProd ? "/robodatapioneers2024_datastory" : "",
-	assetPrefix: isProd ? "/robodatapioneers2024_datastory/" : "",
+	trailingSlash: true,
+	basePath: isProd ? repoName : "",
+	assetPrefix: isProd ? repoName : "",
 };
 
 export default nextConfig;
