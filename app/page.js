7279bc729228fc80f30b98d2f6acf85a367475f0
andres <andresnowak52@gmail.com>
2024-12-17 23:33:03 +0100

    changes

--- app/page.js
+++ app/page.js
@@ -2,8 +2,6 @@
 
 import Image from "next/image";
 import { useEffect, useState } from 'react';
-import Chart from 'chart.js/auto';
-import Papa from "papaparse";
 import { LoadingSpinner, ChartComponent, RacingChartComponent, BubbleChartComponent } from "./data_charts.js";
 import { NavBar } from "./nav.js";
 
@@ -171,6 +169,15 @@ function Footer() {
     );
 }
 
+function SubTitleText({ title, text }) {
+    return (
+        <div className="flex flex-col space-y-2 p-4 max-w-4xl">
+            <h2 className="text-2xl font-bold mb-2">{title}</h2>
+            <p className="text-lg">{text}</p>
+        </div>
+    );
+}
+
 export default function Home() {
     return (
 		<>
@@ -178,7 +185,7 @@ export default function Home() {
 			<LandingPage />
 			<main className="min-h-screen flex flex-col items-center">
 				<div className="flex flex-col space-y-2 p-4 max-w-4xl">
-					<h1 className="text-3xl font-bold mb-2">Create Next App</h1>
+					<h1 className="text-3xl font-bold mb-2">Introduction</h1>
 					<p className="text-lg">
 						Lorem ipsum dolor sit, amet consectetur adipisicing
 						elit. Ipsam autem quibusdam, delectus in corrupti, ab
@@ -208,62 +215,19 @@ export default function Home() {
 						quia veniam nemo voluptatibus animi! Sit labore eius
 						voluptatem hic fugit eum itaque tempora, veniam ipsam at
 						saepe numquam quis error perferendis eos, repellat ex
-						harum excepturi. Quae natus maxime ex eligendi odit
-						molestias explicabo voluptatibus atque! Quis officiis
-						quam, exercitationem blanditiis, dicta alias voluptates
-						accusamus voluptatibus, fugit iste dolorem! Numquam
-						excepturi quia maxime illo ea minus? Tempora eveniet
-						similique non perferendis repudiandae neque commodi
-						aspernatur est totam nobis dolor, delectus incidunt
-						repellendus tenetur facilis et quam, dicta corrupti quas
-						iusto vitae quisquam illum saepe! Molestiae, reiciendis.
-						Possimus quas voluptates, tempora iusto ab perspiciatis
-						atque amet explicabo enim laborum! Alias accusamus,
-						veniam voluptatum sed inventore cupiditate? Reiciendis
-						temporibus veniam autem, quo illum possimus magni
-						blanditiis et fugiat. Quae, voluptates fugit? Excepturi,
-						quos. Accusamus numquam excepturi magnam libero porro
-						eaque quas commodi, dolore facere ipsum aliquid saepe
-						quibusdam. Ipsa deleniti, aperiam assumenda veniam iste
-						cum sapiente voluptatem veritatis.
+						harum excepturi.
 					</p>
 				</div>
 				<ChartComponent loading={<LoadingSpinner />} />
-				<div className="flex flex-col space-y-2 p-4 max-w-4xl">
-					<h2 className="text-2xl font-bold mb-2">Create Next App</h2>
-					<p className="text-lg">
-						Lorem ipsum dolor sit, amet consectetur adipisicing
-						elit. Ipsam autem quibusdam, delectus in corrupti, ab
-						impedit magni iure eveniet aliquid soluta neque quisquam
-						ducimus dolores ex suscipit pariatur. Voluptatibus,
-						exercitationem? Lorem ipsum, dolor sit amet consectetur
-						adipisicing elit. Perspiciatis repellat iure tenetur
-						similique nemo soluta velit voluptate. Tempore esse
-						adipisci quia repellat amet eaque numquam deleniti
-						asperiores dolore? Voluptates, possimus? Suscipit fuga
-						laudantium beatae vel esse non, consectetur dolores
-						praesentium eos tempora? Perferendis eius accusantium
-						tenetur sit error natus, corporis voluptas, beatae sed
-						ipsa repudiandae qui nobis autem voluptatum quod? Soluta
-						sequi deleniti et a, expedita nesciunt quos ad, facere
-						laudantium nisi numquam perferendis? Veritatis eum harum
-						saepe quae itaque maxime quia, assumenda vero commodi
-						iste dolores corporis dolorum sequi.
-					</p>
-				</div>
+				<SubTitleText
+                    title="Create Next App"
+                    text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam autem quibusdam, delectus in corrupti, ab impedit magni iure eveniet aliquid soluta neque quisquam ducimus dolores ex suscipit pariatur. Voluptatibus, exercitationem? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis repellat iure tenetur similique nemo soluta velit voluptate."
+                />
 				<RacingChartComponent loading={<LoadingSpinner />} />
-				<div className="flex flex-col space-y-2 p-4 max-w-4xl">
-					<h2 className="text-2xl font-bold mb-2">Create Next App</h2>
-					<p className="text-lg">
-						Lorem ipsum dolor sit, amet consectetur adipisicing
-						elit. Ipsam autem quibusdam, delectus in corrupti, ab
-						impedit magni iure eveniet aliquid soluta neque quisquam
-						ducimus dolores ex suscipit pariatur. Voluptatibus,
-						exercitationem? Lorem ipsum, dolor sit amet consectetur
-						adipisicing elit. Perspiciatis repellat iure tenetur
-						similique nemo soluta velit voluptate.
-					</p>
-				</div>
+				<SubTitleText
+                    title="Create Next App"
+                    text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam autem quibusdam, delectus in corrupti, ab impedit magni iure eveniet aliquid soluta neque quisquam ducimus dolores ex suscipit pariatur. Voluptatibus, exercitationem? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis repellat iure tenetur similique nemo soluta velit voluptate."
+                />
 				<BubbleChartComponent loading={<LoadingSpinner />} />
 			</main>
             <Footer />
